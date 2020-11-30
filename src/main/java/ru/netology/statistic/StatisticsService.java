7f1cd30a16ef3a720e91cf3935e72cf165073921
Irina Anichkova <anichkova2016@yandex.ru>
2020-11-30 23:53:29 +0300

    modified maven.yml

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,8 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
+
+
     /**
      * Calculate index of max income
      *
@@ -11,8 +13,22 @@ public class StatisticsService {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
-            if (current_max < income)
+            if (current_max < income) //noinspection SingleStatementInBlock
+            {
                 current_max = income;
+            }
         return current_max;
+    }
+
+
+    public long findMin(long[] incomesInBillions) {
+        long current_min_index = 0;
+        long incomes = incomesInBillions[0];
+        long current_min = incomesInBillions[0];
+        for (long income : incomesInBillions) {
+            if (current_min >= income)
+            current_min = income;
+        }
+        return current_min;
     }
 }
